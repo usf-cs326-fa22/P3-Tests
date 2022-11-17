9be9167c798de25991eb897a7599da46cfda40a8
Matthew Malensek <matt@malensek.net>
2022-11-16 22:41:13 -0800

    Update documentation test

---
+++ 20-Documentation-1.sh
@@ -0,0 +1,27 @@
+source "${TEST_LIB}/funcs.bash"
+
+test_start "Documentation Check" \
+    "Ensures documentation is provided for each function and data structure and the README is filled out"
+
+if ! ( which doxygen &> /dev/null ); then
+    # "Doxygen is not installed. Please install (as root) with:"
+    # "pacman -Sy doxygen"
+    test_end 1
+fi
+
+# If the grep below finds any TODOs, the test case fails.
+grep 'TODO' "${TEST_DIR}/../README.md" && test_end 1
+
+
+# If we get this far, the README has everything filled out.
+
+
+# Doxygen will look for javadoc-style documentation above each function, struct,
+# etc.
+doxygen "${TEST_DIR}/../Doxyfile" 2>&1 \
+    | grep -v '(variable)' \
+    | grep -v '(macro definition)' \
+    | grep 'is not documented' \
+        && test_end 1
+
+test_end 0
